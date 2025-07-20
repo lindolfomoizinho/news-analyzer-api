c23d0ed75a5045d03fbe55113b6f61253c9cde9f
Lindolfo Moizinho <lindolfomoizinho@gmail.com>
2025-07-19 23:12:18 -0300

    Initial commit

---
+++ src/main/java/br/com/lindolfomoizinho/newsanalyzerapi/NewsAnalyzerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lindolfomoizinho.newsanalyzerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAnalyzerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsAnalyzerApiApplication.class, args);
+    }
+
+}
